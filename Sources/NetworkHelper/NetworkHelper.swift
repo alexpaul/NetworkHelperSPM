9e1a3449b4b109a44a8704bd5875f68c2546a3db
alexpaul <alexpaulnyc@gmail.com>
2019-12-28 06:10:15 -0500

    working on urlcahe

--- Sources/NetworkHelper/NetworkHelper.swift
+++ Sources/NetworkHelper/NetworkHelper.swift
@@ -22,13 +22,54 @@ public class NetworkHelper {
   
   public func performDataTask(with request: URLRequest,
                        completion: @escaping (Result<Data, AppError>) -> ()) {
+    
+    
+    // https://developer.apple.com/documentation/foundation/nsurlrequest/cachepolicy
+    switch request.cachePolicy {
+    case .useProtocolCachePolicy:
+      print("useProtocolCachePolicy")
+    default:
+      print("other policy")
+    }
+    
+    // clear cache base on given criteria e.g number of days since last fetch
+    
+    // How to leverage HTTP cache in iOS
+    // https://www.fabernovel.com/en/engineering/how-to-leverage-http-cache-in-ios
+    
+    // NSHipster - NSURLCache
+    // https://nshipster.com/nsurlcache/
+    
+    // Apple - Accessing cached data
+    // https://developer.apple.com/documentation/foundation/url_loading_system/accessing_cached_data
+    
+    
+    
+    if let cachedResponse = urlSession.configuration.urlCache?.cachedResponse(for: request),
+      let httpURLResponse = cachedResponse.response as? HTTPURLResponse {
+     // let userInfo = cachedResponse.userInfo {
+      
+      let data = cachedResponse.data
+        
+      print("userInfo \(cachedResponse.userInfo)")
+      
+      print("httpURLResponse: \(httpURLResponse)")
+      
+      let currentDate = Date()
+      let cacheDateHeader = currentDate.timeIntervalSince1970
+      print("cacheDateHeader \(cacheDateHeader)")
+      
+      completion(.success(data))
+      return
+    }
+    
     // two states on dataTask, resume() and suspended by default
     // suspended simply won't perform network request
     // this ultimately leads to debugging errors and time lost if
     // you don't explicitly resume() request
     
     let dataTask = urlSession.dataTask(with: request) { (data, response, error) in
-      
+            
       // 1. deal with error if any
       // check for client network errors
       if let error = error {
